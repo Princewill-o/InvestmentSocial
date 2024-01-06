697f8693b93820748be9eedd9c1eb3488b9a8f61
Princewill <91944411+packzpackz@users.noreply.github.com>
2024-01-06 01:10:31 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAbJUirjCOv-5FVnmn5Jm8JZkV0Vr74ec",
+  authDomain: "signuppage-2338e.firebaseapp.com",
+  projectId: "signuppage-2338e",
+  storageBucket: "signuppage-2338e.appspot.com",
+  messagingSenderId: "889790864877",
+  appId: "1:889790864877:web:bd1e0f88f13f310ee11be4",
+  measurementId: "G-Z71KH0M6B3"
+};
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  // coding part through script
+
+  
